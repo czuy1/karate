7d03c4fd9477099d9836241fd4498be16cf14445
Gustavo Czuhajowskyj <gusczuha@gmail.com>
2024-01-08 20:53:03 -0300

    mi primer commit

---
+++ funcion.js
@@ -0,0 +1,12 @@
+function dojo() {
+    location.href ="inoue.html"
+}
+function torneos() {
+    location.href ="torneos.html"
+}
+function federacion() {
+    location.href ="https://karatefak.com.ar/"
+}
+function mundial() {
+    location.href ="https://www.wkf.net/"
+}
